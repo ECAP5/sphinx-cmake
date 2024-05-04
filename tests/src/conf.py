22edcd4c6a47e51bb0a49c95ddc1c47b442ffeda
Clément Chaine <clement.chaine25@gmail.com>
2024-05-04 07:18:18 +0000

    Adds a test project for sphinx bindings

---
+++ tests/src/conf.py
@@ -0,0 +1,24 @@
+import os, sys
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sphinx-cmake'
+copyright = '2024, Clément Chaine'
+author = 'Clément Chaine'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [] 
+templates_path = []
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
